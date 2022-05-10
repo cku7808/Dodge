30b5f190f5767b0d1d0af56c2ce47762a90e5673
최고운 <cku7808@naver.com>
2022-05-10 20:18:41 +0900

    실습-Dodge

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float rotationSpeed = 60f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
+}
